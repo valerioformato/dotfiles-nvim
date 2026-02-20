36ee1b04f17d9216b5320d4e48460b944fabdfe2
Valerio Formato <valerio.formato@cern.ch>
2026-02-20 12:32:06 +0100

    Add format on save

--- lua/vformato/lsp.lua
+++ lua/vformato/lsp.lua
@@ -93,3 +93,10 @@ vim.lsp.config['bashls'] = {
 }
 
 vim.lsp.enable({ 'lua_ls', 'clangd', 'cmake', 'bashls' })
+
+-- Format on save
+vim.api.nvim_create_autocmd('BufWritePre', {
+  callback = function()
+    vim.lsp.buf.format({ async = false })
+  end,
+})
