c671a97d2c23893a4db6011d13ec0361c223535f
Valerio Formato <valerio.formato@roma2.infn.it>
2026-03-17 10:12:28 +0100

    Set default indent size to 2 spaces

--- lua/vformato/init.lua
+++ lua/vformato/init.lua
@@ -4,6 +4,12 @@ vim.g.mapleader = " " -- Set leader key before Lazy
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- Default indentation
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+
 -- Auto-reload files when changed externally
 vim.opt.autoread = true
 vim.api.nvim_create_autocmd({'FocusGained', 'BufEnter', 'CursorHold'}, {
